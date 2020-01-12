3ed1656fe9488413032bb028404423bdbbd41d4e
Het Shah <het29491@gmail.com>
2020-01-12 10:24:29 +0530

    First Commit

---
+++ BoilerPlateProject/src/demo/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls import include, url
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include (debug_toolbar.urls)),
+    ] + urlpatterns
